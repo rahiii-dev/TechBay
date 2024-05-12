31686535ca79fbf0a4e11d54eccbbfcab1843e63
rahiii-dev <rahiofcl@gmail.com>
2024-05-12 21:17:01 +0530

    updated formerror handler

--- src/middleware/formErrorHandler.js
+++ src/middleware/formErrorHandler.js
@@ -1,28 +1,28 @@
 const { validationResult } = require("express-validator");
 
-const handleFormValidation = (renderPagePath, renderPageTiltle = "Tech Bay") => {
-  return (req, res, next) => {
-    const errors = validationResult(req);
+const handleFormValidation = (req, res, next) => {
+  const errors = validationResult(req);
 
-    if (!errors.isEmpty()) {
-      const errorObj = {};
+  if (!errors.isEmpty()) {
+    const errorObj = {};
 
-      errors.array().forEach((error) => {
-          errorObj[error.path + "Error"] = error.msg;
-          errorObj[error.path] = error.value;
-      });
+    errors.array().forEach((error) => {
+      errorObj[error.path + "Error"] = error.msg;
+      errorObj[error.path] = error.value;
+    });
 
-      for (let key in req.body) {
-        if (!errorObj[key] && req.body[key]) {
-          errorObj[key] = req.body[key];
-        }
+    for (let key in req.body) {
+      if (!errorObj[key] && req.body[key]) {
+        errorObj[key] = req.body[key];
       }
-
-      return res.render(renderPagePath, { title: renderPageTiltle, errorObj });
     }
 
-    next();
-  };
-}
+    req.flash("errorObj", errorObj);
+    const reqUrl = req.originalUrl.split("?")[0];
+    return res.redirect(reqUrl);
+  }
+
+  next();
+};
 
 module.exports = handleFormValidation;
