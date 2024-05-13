51d021c7e6837b578608d430357b64ad87b919ea
rahiii-dev <rahiofcl@gmail.com>
2024-05-13 18:26:48 +0530

    updated admin product layout

--- src/router/admin/productRouter.js
+++ src/router/admin/productRouter.js
@@ -3,9 +3,10 @@ const router = require('express').Router();
 const controller = require('../../controller/productController');
 
 router.get('/list', controller.renderProductListPage);
-router.get('/:product_id', controller.renderProductViewPage);
 
 router.get('/add', controller.renderProductAddPage);
+
+router.get('/:product_id', controller.renderProductViewPage);
 
 router.get('/edit/:product_id', controller.renderProductEditPage);
 
